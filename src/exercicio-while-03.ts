6f03cfe0e534f04c1a9d27e2a3d0f49e93923bc7
cezar.mezzalira <cezar.mezzalira@gmail.com>
2025-10-03 00:46:55 +0000

    feat: solve exercise

--- src/exercicio-while-03.ts
+++ src/exercicio-while-03.ts
@@ -5,7 +5,21 @@ import Scanner from "@codeea/scanner"
 
 async function main() {
     const scanner = new Scanner();
-    // Insira a solução aqui
+    // pedir um número pro usuário
+    const numero = await scanner.questionInt("Informe um número para a tabuada: ");
+    // imprimir a tabuada do número
+    // imprimir de 1 até 10
+    let i = 1;
+    // enquanto o valor da variavel i for menor ou igual a 10
+    while (i <= 10) {
+        const resultado = numero * i;
+        // console.log(numero+" x "+i+" = "+resultado);
+        console.log(`${numero} x ${i} = ${resultado}`);
+        // i = i + 1;
+        // i += 1;
+        i++;
+    }
+
     scanner.close();
 }
 
